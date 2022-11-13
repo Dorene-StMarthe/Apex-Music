656d317c244ce140c4fd57aba72a9f71eb66462f
Dorene07 <dstmarth@gmail.com>
2022-11-12 21:07:19 -0500

    modified authentication scopes to function correctly

--- apex/src/App.js
+++ apex/src/App.js
@@ -1,24 +1,10 @@
 import React, {Component}from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Login'
 
-
-class App extends Component {
-
-   
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      music:[]
-    }
-  }
-  render() {
-    return (
-      <div className='App'>
-        <h1>Browse Music</h1>
-        <h3>Create Playlist</h3>
-      </div>
-    );
-  }
+function App() {
+  return <Login />
 }
+
 export default App;
--- apex/src/login.js
+++ apex/src/login.js
@@ -1,11 +1,18 @@
 import React from 'react' 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=5815ab60510246869757e2c94403ae45&response_type=code&redirect_uri=http://localhost:3000&scope=%20user-read-email%20user-read-private%20user-library-read%20user-library-modifiy%20user-read-playback-state%20user-modify-playback-state"
+
+import { Container} from 'react-bootstrap'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=5815ab60510246869757e2c94403ae45&response_type=code&redirect_uri=https://localhost:3000&scope=ugc-image-upload%20user-read-playback-state%20app-remote-control%20user-modify-playback-state%20playlist-read-private%20user-follow-modify%20playlist-read-collaborative%20user-follow-read%20user-read-currently-playing%20user-read-playback-position%20user-library-modify%20playlist-modify-private%20playlist-modify-public%20user-read-email%20user-top-read%20streaming%20user-read-recently-played%20user-read-private%20user-library-read"
+
+
 
 export default function Login() {
     return (
-        <div>
-
-        </div>
+        <Container className="d-flex justify-center align-items-center"
+        style={{minHeight: "1000vh"}}>
+            <a className="btn btn-success btn-lg" href= {AUTH_URL}>
+                Sign in With Spotify
+            </a>
+        </Container>
     )
-
 }
