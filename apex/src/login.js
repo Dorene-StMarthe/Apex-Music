42d80f697af11cea9f5c73414b66b28aa4c9660e
Dorene07 <dstmarth@gmail.com>
2022-11-12 20:20:16 -0500

    set up authentication url and installed bootstrap

---
+++ apex/src/login.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=5815ab60510246869757e2c94403ae45&response_type=code&redirect_uri=http://localhost:3000&scope=%20user-read-email%20user-read-private%20user-library-read%20user-library-modifiy%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+    return (
+        <div>
+
+        </div>
+    )
+
+}
