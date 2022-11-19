c0de2782d3cdfca6fc6b79360e5d69ced07e0108
Dorene07 <dstmarth@gmail.com>
2022-11-19 13:30:39 -0500

    user auth

--- server/server.js
+++ server/server.js
@@ -1,8 +1,28 @@
 const express = require('express');
-const spotifyWebApi = require('spotify-web-api-node');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
+app.post('/refresh', (req,res)=> {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+            redirectUri: 'http://localhost:3000',
+            clientId: '5815ab60510246869757e2c94403ae45',
+            clientSecret: '8fb1c261330c4d06a6720d4d85cb7ebb',
+            refreshToken
+        })
+        spotifyApi.refreshAccessToken().then(
+            (data) =>{
+              console.log(data.body)
+            }).catch(() => {
+            res.sendStatus(400)
+            })
+        })
+       
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
@@ -10,7 +30,7 @@ app.post('/login', (req, res) => {
         clientId: '5815ab60510246869757e2c94403ae45',
         clientSecret: '8fb1c261330c4d06a6720d4d85cb7ebb'
     })
-})
+
 
 spotifyApi.authorizationCodeGrant(code).then(data => {
     res.json({
@@ -18,6 +38,9 @@ spotifyApi.authorizationCodeGrant(code).then(data => {
         refreshToken: data.body.refresh_token,
         expiresIn: data.body.expires_in
     })
-}).catch(()=>{
+}).catch(err =>{
+    console.log(err)
     res.sendStatus(400)
+    })
 })
+app.listen(3001)
